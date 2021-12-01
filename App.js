9e1f2363db306d530ed3fcb9d3ec27cf57e29fee
Joy M <joy.js.mun@gmail.com>
2021-12-01 23:41:31 +0900

    ADD: transition

--- App.js
+++ App.js
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
 
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+const Box = styled.TouchableOpacity`
+  background-color: tomato;
+  width: 200px;
+  height: 200px;
+`;
+
 export default function App() {
-  return null;
+  const [y, setY] = useState(0);
+  const moveUp = () => {
+    setInterval(() => setY((prev) => prev + 20), 500);
+  };
+  return (
+    <Container>
+      <Box
+        onPress={moveUp}
+        style={{
+          transform: [{ translateY: y }],
+        }}
+      ></Box>
+    </Container>
+  );
 }
